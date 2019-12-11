ba568e153093c2494b888f179c4b7e9d26bbe436
Heather Faerber <heatherdfaerber@gmail.com>
2019-12-11 09:37:17 -0700

    Set up remaining parts of class component like state, constructor and rendering

--- src/App/App.js
+++ src/App/App.js
@@ -1,16 +1,32 @@
 import React, { Component } from 'react';
 import './App.css';
+import ResBox from '../ResBox'
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      reservations: []
+    }
+  }
+
+  componentDidMount() {
+    fetch('http://localhost:3001/api/v1/reservations')
+      .then(res => res.json())
+      .then(data => this.setState({ reservations: data }))
+      .catch(error => console.log(error))
+  }
+
   render() {
     return (
       <div className="App">
         <h1 className='app-title'>Turing Cafe Reservations</h1>
         <div className='resy-form'>
-        <p>test</p>
         </div>
         <div className='resy-container'>
-
+          <ResBox
+            reservations={this.state.reservations}
+          />
         </div>
       </div>
     )
